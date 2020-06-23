92c5ba0a0fbc2cc1ef09729e2e18ab538f372653
2960883911cat <2960883911@qq.com>
2020-06-23 15:48:34 +0800

    这是一颗爱心

---
+++ src/day/love.java
@@ -0,0 +1,16 @@
+package day;
+
+public class love {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("    ****    ****");
+		System.out.println("   ******  ******");
+		System.out.println("  ****************");
+		System.out.println("   **************");
+		System.out.println("     **********");
+	    System.out.println("       ******");
+	    System.out.println("         **");
+	}
+
+} 
