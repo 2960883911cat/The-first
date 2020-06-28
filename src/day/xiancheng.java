e8e3da45d1d12af361a95a1d361865b1747eebce
2960883911cat <2960883911@qq.com>
2020-06-28 11:42:09 +0800

    不知道上传成功没有

--- src/day/xiancheng.java
+++ src/day/xiancheng.java
@@ -2,22 +2,21 @@ package day;
 
 public class xiancheng extends Thread {
 	public void run() {
-		for(int i=1;i<=5;i++) {
-			System.out.println(i+"+++多线程+++");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i + "+++多线程+++");
 
-	}
 		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		xiancheng	thread=new xiancheng();
+		xiancheng thread = new xiancheng();
 		thread.start();
-		for(int i=0;i<=10;i++) {
-			System.out.println(i+"---多线程---");
-			
-			
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(i + "---多线程---");
+
 		}
-		
+
 	}
 
 }
